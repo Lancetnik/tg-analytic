091f482a35e89a3507683e32689b1cac410dfcf3
PasNA6713 <diementros@gmail.com>
2022-04-12 18:10:49 +0300

    rm front

--- backend/app/index/static/js/371.0ce9a753.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[371],{8339:function(n,e,r){r.r(e),r.d(e,{default:function(){return d}});var t=function(){var n=this,e=n.$createElement,r=n._self._c||e;return r("v-card",[n._v("Page1")])},u=[],l={},a=l,c=r(1001),f=r(3453),s=r.n(f),o=r(26),i=(0,c.Z)(a,t,u,!1,null,null,null),d=i.exports;s()(i,{VCard:o.Z})}}]);
-//# sourceMappingURL=371.0ce9a753.js.map
--- backend/app/index/static/js/91.312c2426.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[91],{5297:function(n,e,r){r.r(e),r.d(e,{default:function(){return d}});var t=function(){var n=this,e=n.$createElement,r=n._self._c||e;return r("v-card",[n._v("Page3")])},u=[],l={},a=l,c=r(1001),f=r(3453),s=r.n(f),o=r(26),i=(0,c.Z)(a,t,u,!1,null,null,null),d=i.exports;s()(i,{VCard:o.Z})}}]);
-//# sourceMappingURL=91.312c2426.js.map
